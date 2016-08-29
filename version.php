202af04363e2a9576d5fceb2fecf1e8770e974c1
nise81 <niels.seidel@nise81.com>
2016-08-29 23:02:03 +0200

    init commit

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage blocktype-externalvideo
+ * @author     Catalyst IT Ltd
+ * @author     Gregor Anzelj
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$config = new StdClass;
+$config->version = 2015112400;
+$config->release = '1.1.0';
